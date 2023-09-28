0ab5f1c82bef692ebc2e3c6c28874c7775fb7b0f
Matias <116846525+MatiasPiris17@users.noreply.github.com>
2023-09-28 19:32:11 -0300

    firs commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Microservicios
- henry upskilling microservicios
---
+++ characters/src/controllers/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    getCharacter: require("./getCharacters"),
+    createCharacter: require("./createCharacter")
+}
