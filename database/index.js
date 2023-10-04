ace7232713c3015a5070be31d96690d17481d699
matiaspiris17 <matiaslinkedin17@gmail.com>
2023-10-04 08:56:53 -0300

    enrutado database

--- database/index.js
+++ database/index.js
@@ -1,8 +1,6 @@
 const server = require('./src/server')
 const PORT = 8004;
-// const { Character, Film, Planet } = require("./src/database")
 
-// Planet.list().then(res=> console.log(res[0]))
 
 server.listen(PORT, ()=>{
     console.log(`Database service listening on port ${PORT}`)
